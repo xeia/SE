7e5a29eecea72e77607ab69b7e1639649c80f9e5
Tan Xue Si <tanxs93@gmail.com>
2015-09-19 18:52:12 +0800

    improved formatting and readability

--- CE2/src/ce2/textbuddy/cs2103/TextBuddyTest.java
+++ CE2/src/ce2/textbuddy/cs2103/TextBuddyTest.java
@@ -94,8 +94,10 @@ public class TextBuddyTest {
     public void testAdd() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("add hello");
         tb.testExecuteInput(commandParts);
+
         String outputString = String.format(MESSAGE_ADD_SUCCESS, "hello");
         assertEquals("Add success message", outputString + "\r\n", outContent.toString());
     }
@@ -104,8 +106,10 @@ public class TextBuddyTest {
     public void testAddEmpty() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("add");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Add empty message", MESSAGE_ADD_EMPTY + "\r\n", outContent.toString());
     }
 
@@ -113,8 +117,10 @@ public class TextBuddyTest {
     public void testDeleteInvalidCharacter() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("delete x");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Delete invalid character message", MESSAGE_DELETE_INVALID_NUMBER + "\r\n", outContent.toString());
     }
 
@@ -122,8 +128,10 @@ public class TextBuddyTest {
     public void testDeleteNoParam() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("delete");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Delete invalid line message", MESSAGE_DELETE_EMPTY + "\r\n", outContent.toString());
     }
 
@@ -131,8 +139,10 @@ public class TextBuddyTest {
     public void testDeleteMultiple() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("delete 1 2 3");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Delete multiple message", MESSAGE_DELETE_MULTIPLE + "\r\n", outContent.toString());
     }
 
@@ -140,19 +150,24 @@ public class TextBuddyTest {
     public void testDeleteInvalidLine() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("delete 99999");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Delete invalid character message", MESSAGE_DELETE_INVALID_NUMBER + "\r\n", outContent.toString());
     }
 
     @Test
     public void testDelete() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
         String[] commandParts = tb.testProcessCommand("add TESTDELETE");
         tb.testExecuteInput(commandParts);
+
         System.setOut(new PrintStream(outContent));
         commandParts = tb.testProcessCommand("delete 1");
         tb.testExecuteInput(commandParts);
+
         String outputString = String.format(MESSAGE_DELETE_SUCCESS, "TESTDELETE");
         assertEquals("Delete success message", outputString + "\r\n", outContent.toString());
     }
@@ -160,33 +175,45 @@ public class TextBuddyTest {
     @Test
     public void testClear() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
         String[] commandParts = tb.testProcessCommand("add TESTDELETE");
         tb.testExecuteInput(commandParts);
+
         System.setOut(new PrintStream(outContent));
         commandParts = tb.testProcessCommand("clear");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Clear message", MESSAGE_CLEAR + "\r\n", outContent.toString());
     }
 
     // Test for commands that will be deemed as invalid if extra parameters are given
     @Test
     public void testExtraParameters() {
+        // clear command
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         String[] commandParts = tb.testProcessCommand("clear EXTRA");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Invalid parameters message", MESSAGE_PARAMETERS_INVALID + "\r\n", outContent.toString());
 
+        // display command
         outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         commandParts = tb.testProcessCommand("display EXTRA");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Invalid parameters message", MESSAGE_PARAMETERS_INVALID + "\r\n", outContent.toString());
 
+        // exit command
         outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+
         commandParts = tb.testProcessCommand("exit EXTRA");
         tb.testExecuteInput(commandParts);
+
         assertEquals("Invalid parameters message", MESSAGE_PARAMETERS_INVALID + "\r\n", outContent.toString());
     }
 }
